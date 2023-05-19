0efaa885f667c6ac4a8c15b67c902b9febe8430b
AliLordLoss <a.a.ghanati@gmail.com>
2023-05-19 10:15:29 +0330

    -added MiniJava project files

---
+++ MiniJava/src/main/java/errorHandler/ErrorHandler.java
@@ -0,0 +1,14 @@
+package errorHandler;
+
+/**
+ * Created by Alireza on 6/28/2015.
+ */
+
+public class ErrorHandler {
+    public static boolean hasError = false;
+
+    public static void printError(String msg) {
+        hasError = true;
+        System.out.println(msg);
+    }
+}
